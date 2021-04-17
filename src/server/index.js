c44d218cd323c6eecf75125e9995a438f4124c6f
Nashwa Emad Ali <nashwaemad2516@gmail.com>
2021-04-17 04:33:02 +0200

    add files and remove

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`);
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -14,8 +17,8 @@ app.get('/', function(req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function() {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function() {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function(req, res) {
